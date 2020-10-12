3786d4374fab7551a3306af0e06f290a30f42290
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2020-10-12 15:13:00 +0100

    update

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - [ ] single dev.sh running everything (webpack build and dev server)
 - [ ] refresh param ?_=time (browser cache)
 - [ ] extend webpack to be able to import semantic ui css
-- [ ] 
+- [ ] parameter to skip cache-loader in webpack.config.js for docker image build mode
 - [ ] 
 
 
---
+++ app/views/Projects/Project.js
@@ -0,0 +1,153 @@
+
+import React, {
+  useEffect,
+  useState,
+  useRef,
+  useContext,
+} from 'react';
+
+import './Project.scss';
+
+import log from 'inspc';
+
+import {
+  Breadcrumb,
+  List,
+  Button,
+  Icon,
+  Form,
+  Checkbox,
+  Loader,
+  Modal,
+  Header,
+  Dropdown,
+} from 'semantic-ui-react';
+
+import {
+  Link,
+  useHistory,
+  useParams,
+} from 'react-router-dom';
+
+import {
+  StoreContext as StoreContextProjects,
+
+  actionProjectsFormPopulate,
+  actionProjectsFormFieldEdit,
+  actionProjectsFormSubmit,
+
+  getProjectForm,
+  getProjectFormErrors,
+} from '../../_storage/storeProjects';
+
+import {
+  StoreContext as StoreContextNotifications,
+
+  notificationsAdd,
+} from '../../components/Notifications/storeNotifications';
+
+export default function Project() {
+
+  const { id } = useParams();
+
+  useContext(StoreContextProjects);
+
+  useContext(StoreContextNotifications);
+
+  const [ loading, setLoading ] = useState(true);
+
+  const [ sending, setSending ] = useState(false);
+
+  const history = useHistory();
+
+  const form = getProjectForm();
+
+  const errors = getProjectFormErrors();
+
+  useEffect(() => {
+
+    return actionProjectsFormPopulate({
+      id,
+      onLoad: ({
+        form,
+        errors = {},
+        submitted,
+      }) => {
+        setLoading(false);
+        setSending(false);
+
+        if (submitted) {
+
+          if (Object.keys(errors).length === 0) {
+
+            history.push(`/`);
+
+            notificationsAdd(`Project '<b>${form.name}</b>' have been ${id ? 'edited': 'created'}`)
+          }
+          else {
+
+            notificationsAdd(`Validation error has been detected, please check the data in the form and submit again.`, 'error');
+          }
+        }
+      }
+    })
+
+  }, []);
+
+  function onSubmit() {
+
+    setSending(true);
+
+    actionProjectsFormSubmit({
+      form,
+    });
+  }
+
+  return (
+    <div className="project-edit">
+      <Breadcrumb>
+        <Breadcrumb.Section
+          // onClick={loginSignOut}
+          size="mini"
+          as={Link}
+          to="/"
+        >Dashboard</Breadcrumb.Section>
+        <Breadcrumb.Divider />
+        <Breadcrumb.Section>{id ? `Edit project "${form.name}"`: `Create project`}</Breadcrumb.Section>
+      </Breadcrumb>
+      <hr />
+      <div>
+
+        {loading ? (
+          `Loading...`
+        ) : (
+          <Form onSubmit={onSubmit}
+                autoComplete="off"
+          >
+            <Form.Field
+              disabled={loading}
+              error={!!errors.name}
+            >
+              <label>Name</label>
+              <input placeholder='Name' value={form.name}
+                     onChange={e => actionProjectsFormFieldEdit('name', e.target.value)}
+                     autoComplete="nope"
+              />
+              {errors.name && <div className="error">{errors.name}</div>}
+            </Form.Field>
+            <Form.Field disabled={sending}>
+              <Button type='submit'
+                      autoComplete="nope"
+              >
+                {form.id ? 'Save changes' : 'Create'}
+              </Button> {sending && `Sending data...`}
+            </Form.Field>
+          </Form>
+        )}
+
+      </div>
+    </div>
+  );
+}
+
+
