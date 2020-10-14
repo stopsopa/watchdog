b3118887d7777bf568b2886fad993e3f9599ed72
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2020-10-14 09:31:18 +0100

    o

--- app/views/Probes/example-active.js
+++ app/views/Probes/example-active.js
@@ -28,7 +28,7 @@ module.exports = async function () {
     return { // this object should have only two keys: 'proble' and 'data' (where data is optional)
 
       // This is the only field used to determine if probe passed the test,
-      // rest of data (from 'optionalData' field) goes straight to the elasticsearch logs as it is.
+      // rest of data (from 'optionalLog' field) goes straight to the elasticsearch logs as it is.
 
       // Try to don't put too much data there - be reasonable
 
@@ -38,7 +38,7 @@ module.exports = async function () {
       probe: data.status === 200,
 
 
-      optionalData: data, // optionalData - optional field
+      optionalLog: data, // optional field
     }
 
   }
@@ -48,9 +48,9 @@ module.exports = async function () {
 
     return {
       probe: false, // still "probe" key with boolean value type is required
-      optionalData: {
+      optionalLog: {
 
-        // if it reached catch then try to use always 'catch' key in 'optionalData' in es
+        // if it reached catch then try to use always 'catch' key in 'optionalLog' in es
         // to store catch error messages the same way for general consistancy of future queries,
         // to be able filter out all results that falled into catch for example
         catch: serializeError(e), // Casting exception to string (which will happen implicitly) is not extracting all available data from it, use serializeError()
--- app/views/Projects/ProjectsList.js
+++ app/views/Projects/ProjectsList.js
@@ -1,6 +1,4 @@
 
-/* eslint-disable */
-
 import React, {
   useEffect,
   useState,
