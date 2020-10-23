cd01890f51bf38dd5b1319f038f7ce15c2a6b7fe
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2020-10-23 03:00:34 +0100

    .env.dist

--- webpack.sh
+++ webpack.sh
@@ -3,10 +3,10 @@ _ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
 
 node "$_ROOT/bash/node/versioncheck.js" --nvmrc .nvmrc --exact
 
-if ! [ -f "$_ROOT/.env" ]; then
-
-  cp "$_ROOT/.env.dist" "$_ROOT/.env"
-fi
+#if ! [ -f "$_ROOT/.env" ]; then
+#
+#  cp "$_ROOT/.env.dist" "$_ROOT/.env"
+#fi
 
 if ! [ -f "$_ROOT/.env" ]; then
 
