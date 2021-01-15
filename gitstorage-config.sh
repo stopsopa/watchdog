707c1ff2c93011cf1777e5584661e1ca223451ac
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2021-01-15 17:32:52 +0000

    backup .env

--- gitstorage-config.sh
+++ gitstorage-config.sh
@@ -10,6 +10,7 @@ GITSTORAGETARGETDIR="watchdog"
 GITSTORAGELIST=(
     ".env::$GITSTORAGETARGETDIR/.env"
     ".env.kub.test::$GITSTORAGETARGETDIR/.env.kub.test"
+    "docker/kbuild/backup/.env::$GITSTORAGETARGETDIR/.env.backup"
     "gitstorage-config.sh::$GITSTORAGETARGETDIR/gitstorage-config.sh"
     "sensitive.txt::$GITSTORAGETARGETDIR/sensitive.txt"
 )
